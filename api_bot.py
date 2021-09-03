84f3f5f2bb46ab46451e5465e40f6f901bc9d93a
ZorkoAnastasiya <asskarra@gmail.com>
2021-09-03 16:42:36 +0300

    adding new functions and classes

--- api_bot.py
+++ api_bot.py
@@ -1,11 +1,37 @@
 # coding=utf-8
 import TelegramBot
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from typing import Optional
+
 
 app = FastAPI()
 
 
-@app.get("/tb/about")
-def _():
-    response = TelegramBot.get_me()
-    return response
+@app.get("/tg/about")
+async def _():
+    user = TelegramBot.get_me()
+    return user
+
+
+@app.get("/tg/webhook")
+async def _():
+    whi = TelegramBot.get_webhook_info()
+    return whi
+
+
+@app.post("/tg/webhook")
+async def _(
+        whi: TelegramBot.WebhookInfo,
+        x_secret_key: Optional[str] = Header(None),
+        authorization: Optional[str] = Header(None)
+):
+    key = TelegramBot.key_verification(x_secret_key)
+    if not key:
+        return {"Message": "No access"}
+    else:
+        auth = TelegramBot.entrance(authorization)
+        if not auth:
+            return {"Message": "No rights"}
+        else:
+            result = TelegramBot.set_webhook(whi)
+            return result
